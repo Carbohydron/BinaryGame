f9f9f525f1308b6ab6ad419a8f505275639a4d22
colson <colson1@trinity.edu>
2014-10-27 20:30:36 -0500

    monday's work before study session

--- WarioWare/Assets/Scripts/BinaryBlockRow.cs
+++ WarioWare/Assets/Scripts/BinaryBlockRow.cs
@@ -6,11 +6,15 @@ public class BinaryBlockRow : MonoBehaviour
 {
 		NumBlock[] blockarr = new NumBlock[8];
 		Canvas c;
+		GameObject goaltxt;
 		GameObject txt;
 		RectTransform txtrt;
 		private Text rowtotal;
 		public bool hard;
 		RectTransform rowtransform;
+
+		int goalnum = 1;
+		int currentVal = 0;
 		
 		void Start ()
 		{	
@@ -22,23 +26,59 @@ public class BinaryBlockRow : MonoBehaviour
 						txtrt = txt.GetComponent<RectTransform> ();
 						txtrt.localPosition = new Vector3 (rowtransform.localPosition.x + rowtransform.rect.width / 2 + txtrt.rect.width / 2 + 5, rowtransform.localPosition.y - 5, 0);
 						txtrt.localScale = new Vector3 (1, 1, 1);
-				
+						
 						rowtotal = txt.GetComponent<Text> ();
 				}
 				blockarr = gameObject.GetComponentsInChildren<NumBlock> ();
+				
 
+				goaltxt = Instantiate (Resources.Load ("txt", typeof(GameObject))) as GameObject;
+				goaltxt.GetComponent<RectTransform> ().parent = c.transform;
+				
+				RectTransform goaltxtrt = goaltxt.GetComponent<RectTransform> ();
+			
+				goaltxtrt.localPosition = new Vector3 (txtrt.localPosition.x + txtrt.rect.width + 5, txtrt.localPosition.y, 0);
+				goaltxtrt.localScale = new Vector3 (1, 1, 1);
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
-				
-				int num = 0;
+
+				Text goaltxttemp = goaltxt.GetComponent<Text> ();
+				goaltxttemp.text = goalnum.ToString ();
+			
+				currentVal = 0;
 				for (int a =0; a<8; ++a) {
 						if (blockarr [a].getValue () == 1) {
-								num += (int)System.Math.Pow (2, a);
+								currentVal += (int)System.Math.Pow (2, a);
 						}
 				}
-				rowtotal.text = num.ToString ();
+				if (!hard) {
+						rowtotal.fontStyle = FontStyle.Normal;
+						rowtotal.text = currentVal.ToString ();
+				}
+			
+		}
+
+		private void rowSolved ()
+		{
+			
+		}
+
+		public int Goalnum {
+				get {
+						return goalnum;
+				}
+
+				set {
+						goalnum = value;
+				}
+		}
+
+		public int CurrentVal {
+				get {
+						return currentVal;
+				}
 		}
 }
--- WarioWare/Assets/Scripts/BinaryGame.cs
+++ WarioWare/Assets/Scripts/BinaryGame.cs
@@ -3,16 +3,33 @@ using System.Collections;
 
 public class BinaryGame : MonoBehaviour
 {
-
+		private bool difficult = false;
 		public GameObject blockPrefab;
+		BinaryBlockRow[] blockrows = new BinaryBlockRow[1];
 		
 		void Start ()
 		{
+				blockrows [0] = Instantiate (Resources.Load ("BinaryBlockRow", typeof(GameObject))) as BinaryBlockRow;
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
-	
+				if (!difficult) {
+					
+				}
+
+				for (int a =0; a<blockrows.Length; ++a) {
+						if (blockrows [a].CurrentVal == blockrows [a].Goalnum) {
+								rowSolved (a);
+						}
+				}
 		}
+
+		private void rowSolved (int a)
+		{
+
+		}
+
+		
 }
