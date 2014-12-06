dcc914fa8c38f98b71f5c2e33c7a1b7ff151f6bc
colson <colson1@trinity.edu>
2014-12-06 13:47:23 -0600

    Added gameover

--- WarioWare/Assets/Scripts/BinaryGame.cs
+++ WarioWare/Assets/Scripts/BinaryGame.cs
@@ -4,10 +4,12 @@ using System.Collections;
 
 public class BinaryGame : MonoBehaviour
 {
-		public bool difficult = false;
+		public bool hard = false;
+		public bool medium = false;
 		BinaryBlockRow[] blockrows = new BinaryBlockRow[5];
 
 		bool init = false;
+		private bool gameoverbool = false;
 		public GameObject score;
 		public GameObject time;
 		Text scoretxt;
@@ -39,40 +41,54 @@ public class BinaryGame : MonoBehaviour
 		// Update is called once per frame
 		void Update ()
 		{
-				timenum -= Time.deltaTime;
-				if (!init) {
-						init = true;
-						for (int a =0; a< blockrows.Length; ++a) {
-								if (blockrows [a] != null) {
-										Vector3 tempv = new Vector3 (-100, -220 + a * 100, 0);
-										blockrows [a].updatePos (tempv);
-								}
-						}
-				}
-			
-				scoretxt.text = scorenum.ToString ("0000");
-				timetxt.text = timenum.ToString ("00");
-
-
-				for (int a =0; a<blockrows.Length; ++a) {
-						if (blockrows [a] != null) {
-								Vector3 temp = new Vector3 (-100, -220 + a * 100, 0);
-								//blockrows [a].updatePos (temp);
-
-								blockrows [a].Rowtransform.localPosition = Vector3.Lerp (blockrows [a].Rowtransform.localPosition, temp, Time.deltaTime);
-								blockrows [a].Txtrt.localPosition = Vector3.Lerp (blockrows [a].Txtrt.localPosition, new Vector3 (temp.x + blockrows [a].Rowtransform.rect.width / 2 + blockrows [a].Txtrt.rect.width / 2 + 5, temp.y - 5, 0), Time.deltaTime);
-								blockrows [a].Goaltxtrt.localPosition = Vector3.Lerp (blockrows [a].Goaltxtrt.localPosition, new Vector3 (blockrows [a].Txtrt.localPosition.x + blockrows [a].Txtrt.rect.width + 5, temp.y, 0), Time.deltaTime);
-								blockrows [a].updateDifficulty (difficult);
-						}
-				}
+				if (!gameoverbool) {
+						timenum -= Time.deltaTime;
 				
-				for (int a =0; a<blockrows.Length; ++a) {
-						//Debug.Log (a.ToString () + " val: " + blockrows [a].CurrentVal.ToString ());
-						if (blockrows [a] != null) {
-								if (blockrows [a].CurrentVal == blockrows [a].Goalnum) {
-										rowSolved (a);
+
+						if (!init) {
+								init = true;
+								for (int a =0; a< blockrows.Length; ++a) {
+										if (blockrows [a] != null) {
+												Vector3 tempv = new Vector3 (-100, -220 + a * 100, 0);
+												blockrows [a].updatePos (tempv);
+										}
 								}
 						}
+			
+						scoretxt.text = scorenum.ToString ("0000");
+						timetxt.text = timenum.ToString ("00");
+
+
+						for (int a =0; a<blockrows.Length; ++a) {
+								if (blockrows [a] != null) {
+										Vector3 temp = new Vector3 (-100, -220 + a * 100, 0);
+										//blockrows [a].updatePos (temp);
+
+										blockrows [a].Rowtransform.localPosition = Vector3.Lerp (blockrows [a].Rowtransform.localPosition, temp, Time.deltaTime);
+										blockrows [a].Txtrt.localPosition = Vector3.Lerp (blockrows [a].Txtrt.localPosition, new Vector3 (temp.x + blockrows [a].Rowtransform.rect.width / 2 + blockrows [a].Txtrt.rect.width / 2 + 5, temp.y - 5, 0), Time.deltaTime);
+										blockrows [a].Goaltxtrt.localPosition = Vector3.Lerp (blockrows [a].Goaltxtrt.localPosition, new Vector3 (blockrows [a].Txtrt.localPosition.x + blockrows [a].Txtrt.rect.width + 5, temp.y, 0), Time.deltaTime);
+										blockrows [a].updateDifficulty (medium);
+								}
+						}
+				
+						for (int a =0; a<blockrows.Length; ++a) {
+								//Debug.Log (a.ToString () + " val: " + blockrows [a].CurrentVal.ToString ());
+								if (blockrows [a] != null) {
+										if (blockrows [a].CurrentVal == blockrows [a].Goalnum) {
+												rowSolved (a);
+										}
+								}
+						}
+
+						if (hard) {
+								if (blockrows.Length <= 0) {
+										gameOver ();					
+								}
+						} else if (timenum <= 0 || blockrows.Length <= 0) {
+								gameOver ();
+						}
+				} else {
+
 				}
 		}
 
@@ -85,6 +101,12 @@ public class BinaryGame : MonoBehaviour
 				Invoke ("shiftRows", time);
 				scorenum += timenum;
 
+		}
+
+		private void gameOver(){
+			gameoverbool = true;
+			
+			
 		}
 
 		private void shiftRows ()
